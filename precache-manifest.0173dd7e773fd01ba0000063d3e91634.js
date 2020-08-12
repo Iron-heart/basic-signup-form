19f7bd5eb59bf398994ced4a4f5ac44abd49d240
iron-heart <aironheart@outlook.com>
2020-08-12 19:55:07 +0300

    Updates

---
+++ precache-manifest.0173dd7e773fd01ba0000063d3e91634.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef8baede4d50fd520cbcd1afab2cef2a",
+    "url": "/basic-signup-form/index.html"
+  },
+  {
+    "revision": "98c8bf3fd7255fd3cda9",
+    "url": "/basic-signup-form/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8adc40ecc0b8364071c9",
+    "url": "/basic-signup-form/static/js/2.2e694a83.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/basic-signup-form/static/js/2.2e694a83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98c8bf3fd7255fd3cda9",
+    "url": "/basic-signup-form/static/js/main.7ab33e5d.chunk.js"
+  },
+  {
+    "revision": "fd6a65f47f1d2afdc0b5",
+    "url": "/basic-signup-form/static/js/runtime-main.f32e212c.js"
+  },
+  {
+    "revision": "25e91ac679bb999002e3d2b2f422d0b4",
+    "url": "/basic-signup-form/static/media/moon.25e91ac6.svg"
+  },
+  {
+    "revision": "fe4053b9ad1356c378185ae6c36b2845",
+    "url": "/basic-signup-form/static/media/sun.fe4053b9.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/basic-signup-form/precache-manifest.0173dd7e773fd01ba0000063d3e91634.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/basic-signup-form/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
